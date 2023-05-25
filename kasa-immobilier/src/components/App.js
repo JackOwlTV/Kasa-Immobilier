8e1ba6eb61c2a3cdd125f9c151f19be42538deb0
JackOwlTV <dylan.vivant@sfr.fr>
2023-05-25 15:31:55 +0200

    Mise en place des routes

--- kasa-immobilier/src/components/App.js
+++ kasa-immobilier/src/components/App.js
@@ -1,11 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import Home from './Home/home'
+import Home from './Home/home';
+import Error from './Error/error';
+import About from './About/about';
+import Logement from './Logement/logement'
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/error404' element={<Error/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/logement' element={<Logement/>} />
       </Routes>
     </div>
   );
