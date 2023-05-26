3458ce90775f1acec4a1d4af95688f01f237aeb1
JackOwlTV <dylan.vivant@sfr.fr>
2023-05-26 09:36:47 +0200

    Création et mise en place de la page Error

--- kasa-immobilier/src/page/error.js
+++ kasa-immobilier/src/page/error.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import '../styles/pages/Error.css'
 
 export default function error() {
   return (
-    <div>error</div>
+    <div>
+      <main className='error404'>
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link to={"/"}>
+           <span> Retourner sur la page d'accueil</span>
+        </Link>
+      </main>
+    </div>
   )
 }
