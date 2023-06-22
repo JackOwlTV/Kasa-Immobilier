16fa701e51fb3c7054bee71acb75fed561589099
JackOwl <dylan.vivant@sfr.fr>
2023-06-22 14:28:12 +0200

    update

--- src/page/about.js
+++ src/page/about.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import Banner from '../components/banner'
-import CollapseZone from '../components/collapseZone'
+import Collapse from '../components/Collapse'
 import Footer from '../components/footer'
+import '../styles/components/CollapseZone.css'
+
 
 export default function about() {
+  const dropValue = [
+    {
+      label: "Fiablité",
+      description:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+    },
+    {
+      label: "Respect",
+      description:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    },
+    {
+      label: "Service",
+      description:
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+    },
+    {
+      label: "Sécurité",
+      description:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+    },
+  ]
   return (
     <div>
       <Banner imgSrc="../../assets/banner-about.png" />
-      <CollapseZone />
+      <ul className='drop-down-zone'>
+        {dropValue.map((e, index) => (
+          <Collapse key={index} label={e.label} description={e.description} />
+        ))}
+      </ul>
       <Footer />
     </div>
   )
--- src/page/error.js
+++ src/page/error.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../styles/pages/Error.css'
 
 export default function error() {
@@ -9,7 +9,7 @@ export default function error() {
         <h1>404</h1>
         <p>Oups! La page que vous demandez n'existe pas.</p>
         <Link to={"/"}>
-           <span> Retourner sur la page d'accueil</span>
+          <span> Retourner sur la page d'accueil</span>
         </Link>
       </main>
     </div>
